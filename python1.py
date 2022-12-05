57fdb80b71f35ac5f577c56c550c610c59fbc757
Shikhanchakraborty <shikhanchakraborty8910@gmail.com>
2022-12-05 11:05:11 +0600

    firs evert commit

---
+++ python1.py
@@ -0,0 +1,31 @@
+'''
+# Input phase
+first_name= input("First Name: ")
+last_name= input("Last Name: ")
+# Process
+username=first_name + "@" + last_name
+# Output phase
+print("Username: ",username) 
+'''
+
+'''
+# Slicing
+print("Shikhan Kakan ")
+name="Shikhan Kakan"
+print(name[1])
+# Negative Slicing
+name="Shikhan Kakan"
+print(name[-4:-5]) # it is wrong no result
+'''
+
+# String Methods
+
+name="Shikhan"
+print(name.lower())
+
+
+# Number complex method
+com=2+3j
+print(com)
+print(com.real)
+print(com.imag)
